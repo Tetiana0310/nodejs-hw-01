c53f74a57ce7e7696ad3af6099f30069811138ba
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-11-08 15:27:38 +0200

    Done project

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,17 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeAllContacts = async () => {
+    try {
+        let dbContacts = await readContacts();
+
+        dbContacts = [];
+
+        await writeContacts(dbContacts);
+    }
+    catch (err) {
+        console.error(err);
+}
+};
 
 removeAllContacts();
